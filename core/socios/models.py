2db0c52f554d37c840f5521228105283e48404d5
Vinicius Nunes <viniciusnunes02612@gmail.com>
2024-07-16 03:27:29 -0300

    alterando campos com valor para blanck true

--- core/socios/models.py
+++ core/socios/models.py
@@ -3,20 +3,20 @@ from django.db import models
 # Create your models here.
 
 class Cargo(models.Model):
-    nome_cargo = models.CharField(verbose_name="Cargo",max_length=30, blank=True)
+    nome_cargo = models.CharField(verbose_name="Cargo",max_length=30, blank=False)
 
     def __str__(self):
         return self.nome_cargo
     
 class Socio(models.Model):
-    nomeCompleto = models.CharField(verbose_name="Nome",max_length=50, blank=True)
+    nomeCompleto = models.CharField(verbose_name="Nome",max_length=50, blank=False)
     cpf = models.CharField(verbose_name="CPF", max_length=11, blank=True)
-    dataNascimento = models.DateField(verbose_name="Data de Nascimento", blank=True)
-    sexo = models.CharField(verbose_name="Sexo", max_length=10, blank=True,null=False)
-    registro = models.CharField(verbose_name="RG", max_length=11, blank=True,null=False)
+    dataNascimento = models.DateField(verbose_name="Data de Nascimento", blank=False)
+    sexo = models.CharField(verbose_name="Sexo", max_length=10, blank=False,null=False)
+    registro = models.CharField(verbose_name="RG", max_length=11, blank=False,null=False)
     arq_autoDeclaracao = models.FileField(verbose_name="Auto Declaração",upload_to="declaracoes",null=False)
-    atividade_agr =  models.CharField(verbose_name="Mantem atividade agricola na comunidade", max_length=4,null=False, blank=True)
-    quantidade_pessoa = models.IntegerField(verbose_name="Quantidade de pessoas na familia", blank=True)
+    atividade_agr =  models.CharField(verbose_name="Mantem atividade agricola na comunidade", max_length=4,null=False, blank=False)
+    quantidade_pessoa = models.IntegerField(verbose_name="Quantidade de pessoas na familia", blank=False)
     cargo = models.ForeignKey(Cargo, verbose_name="Cargo", on_delete=models.DO_NOTHING,null=False)
     situacao = models.BooleanField(verbose_name="Situação Atual", default=True,null=False)
     def __str__(self):
@@ -25,8 +25,8 @@ class Socio(models.Model):
 
 class Mensalidade(models.Model):
     nome_socio = models.ForeignKey(Socio,on_delete=models.CASCADE)
-    descricao = models.CharField(verbose_name="Descrição", max_length=50, blank=True,null=False, default="Mensalidade")
-    valor = models.FloatField(verbose_name="Valor", blank=True,null=False)
-    reference_data = models.DateField(verbose_name="Mês de cobrança",blank=True,null=False)
-    data_validade = models.DateField(verbose_name="Mês de Vencimento", blank=True,null=False)
+    descricao = models.CharField(verbose_name="Descrição", max_length=50, blank=False,null=False, default="Mensalidade")
+    valor = models.FloatField(verbose_name="Valor", blank=False,null=False)
+    reference_data = models.DateField(verbose_name="Mês de cobrança",blank=False,null=False)
+    data_validade = models.DateField(verbose_name="Mês de Vencimento",default=False, blank=False,null=False)
     status = models.BooleanField(verbose_name='Status', default=False,null=False)
