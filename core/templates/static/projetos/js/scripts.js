888eb9f9c1fed1fc940d76e754faeadfa0803133
Vinicius Nunes <viniciusnunes02612@gmail.com>
2024-07-16 03:30:23 -0300

    criando e alterando formas de visualizar paginas distintas na home

---
+++ core/templates/static/projetos/js/scripts.js
@@ -0,0 +1,12 @@
+    
+    
+$(document).ready(function() {
+      $('#cadastrar').click(function() {
+        $('#alerta').addClass('show'); // Mostra a notificação
+    
+        // Remove a notificação após 5 segundos
+        setTimeout(function() {
+          $('#alerta').removeClass('show');
+        }, 4000);
+      });
+});
