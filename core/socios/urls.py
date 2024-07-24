c91302f5ba23151415b9c08ea0f194b48b940756
Vinicius Nunes <viniciusnunes02612@gmail.com>
2024-07-24 09:03:39 -0300

    update:criando rota para deletar socio, ver dados do socio e editar

--- core/socios/urls.py
+++ core/socios/urls.py
@@ -7,8 +7,9 @@ urlpatterns = [
     path('', views.pageSocio, name='socios'),
     path('add_socio/', views.addPartners, name='add_socio'),
     path('search_socio/', views.searchSocio, name='BuscarSocio'),
-    path('edit_socio/<int:id>/edit', views.edit_socio, name='editSocio'),
-    path('delete_socio/<int:id>/delete', views.delete_socio, name='deleteSocio'),
+    path('edit_socio/<int:id>/', views.edit_socio, name='editSocio'),
+    path('delete_socio/<int:id>/', views.delete_socio, name='deleteSocio'),
+    path('views_socio/<int:id>/', views.view_socio, name='viewSocio'),
     path('mensalidade/', views.monthly_payment, name='mensalidade'),
 
 ]
