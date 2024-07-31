8d029e618201fb86b5ef5533284e84a542cd6e4f
Vinicius Nunes <viniciusnunes02612@gmail.com>
2024-07-30 22:29:43 -0300

    Atualizando models de Projeto para salvar arquivo na pasta de projetos

--- core/projeto/models.py
+++ core/projeto/models.py
@@ -6,13 +6,13 @@ class Projeto(models.Model):
     titulo = models.CharField(verbose_name="Titulo do Projeto", max_length=100, blank=False, null=False)
     nome_instituicao = models.CharField(verbose_name="Nome da instituicao", max_length=100, blank=False, null=False)
     cnpj = models.CharField(verbose_name="CNPJ",blank=False, max_length=14)
-    arquivo_projeto = models.FileField(verbose_name="Documento do Projeto", upload_to="./projetos", null=False)
+    arquivo_projeto = models.FileField(verbose_name="Documento do Projeto", upload_to="projetos/", null=False)
     data_inicio = models.DateField(verbose_name="Data de inicio", blank=True,null=True)
     data_final = models.DateField(verbose_name="Data de inicio", blank=True, null=True)
     CATEGORIA_CHOICES = [
         ('Esportes', 'Esportes'),
         ('Educacao', 'Educação'),
-        ('Cultura', 'Cultura'),
+        ('Cultura', 'Cultura'), 
         ('Pesquisa', 'Pesquisa'),
         ('Projeto Social', 'Projeto Social'),
         ('Musica', 'Música'),
@@ -23,5 +23,5 @@ class Projeto(models.Model):
 
 
     def __str__(self):
-        return self.titulo
+         return self.titulo
     
